2944b440b70dbd6e3a5be39881317b12e84b7806
[ChangYeon-Lee] <[dlckddus0208@naver.com]>
2021-10-10 23:59:27 +0900

    2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let gul = document.getElementsByClassName("Gul")[0];
 
 function Gon() {
     if (GlobalOnOff == 0) {
+        console.log("성공");
         gul.style.display = "block";
         GlobalOnOff = 1;
     } else {
@@ -10,3 +11,13 @@ function Gon() {
         GlobalOnOff = 0;
     }
 };
+
+// let addhidden = document.getElementsByClassName("addhidden")[0];
+// let NCli = document.getElementsByClassName("nav_controller")[0].getElementsByTagName("li")[0];
+
+// function addshow() {
+//     NCli.addEventListener("mouseover", function() {
+//         addhidden.toggleAttribute("addshow");
+//         console.log("성공");
+//     });
+// };
